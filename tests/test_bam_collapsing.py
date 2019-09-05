8bbacbe8f291e83d1cce452aaadb089df3ce911f
Ronak Shah <rons.shah@gmail.com>
2019-09-05 16:15:15 -0400

    MOdifying test

--- tests/test_bam_collapsing.py
+++ tests/test_bam_collapsing.py
@@ -6,13 +6,64 @@
 import os
 import pytest
 import filecmp
+import subprocess
+import shutil
+
+RESULT_FILE_NAME = ["chr14-intervals-without-duplicates.txt",
+                    "chr14-intervals.txt",
+                    "chr14-pileup-without-duplicates.txt",
+                    "chr14_R1_fastq.gz",
+                    "chr14_R2_fastq.gz",
+                    "chr14_unfiltered_srt_abra_fm-duplex.bai",
+                    "chr14_unfiltered_srt_abra_fm-duplex.bam",
+                    "chr14_unfiltered_srt_abra_fm-duplex_alignment_metrics.txt",
+                    "chr14_unfiltered_srt_abra_fm-simplex.bai",
+                    "chr14_unfiltered_srt_abra_fm-simplex.bam",
+                    "chr14_unfiltered_srt_abra_fm-simplex_alignment_metrics.txt",
+                    "chr14_unfiltered_srt_abra_fm.bai",
+                    "chr14_unfiltered_srt_abra_fm.bam",
+                    "chr14_unfiltered_srt_abra_fm_alignment_metrics.txt",
+                    "collapsed_R1_.fastq",
+                    "collapsed_R2_.fastq",
+                    "goodls",
+                    "second-pass-alt-alleles.txt",
+                    "second-pass-insertions.txt",
+                    "test_bam_collapsing.tar.gz"
+                    ]
 
 
-def test_confirm_cwltool_run(cwltool_setup):
-    assert cwltool_setup.returncode == 0
+def setup_module():
+    """Test the workflow with cwltool"""
+    print('SETUP')
 
+    cmd = [
+        "cwltool",
+        "--preserve-environment",
+        "PATH",
+        "bam_collapsing.cwl",
+        "test_bam_collapsing/test_input/inputs.yaml",
+    ]
+    process = subprocess.run(cmd)
+    return process
 
-def test_check_metrics_file_exists(cwltool_setup):
+'''
+def teardown_module():
+    """
+    Delete output files
+    """
+    print('TEARDOWN')    
+    for outfile in RESULT_FILE_NAME:
+        try:
+            os.remove(outfile)
+        except OSError as e:
+            print("ERROR: cannot remove output file, %s: %s" % (outfile, e))
+    try:
+        shutil.rmtree(os.path.join("test_bam_collapsing","/"))
+    except OSError as e:
+        print("ERROR: cannot remove folder test_bam_collapsing : %s" % (e))
+
+'''
+def test_check_metrics_file_exists():
     assert os.path.exists(
         "chr14_unfiltered_srt_abra_fm_alignment_metrics.txt"
     ), "File does not exist!"
@@ -24,7 +75,7 @@ def test_check_metrics_file_exists(cwltool_setup):
     ), "File does not exist!"
 
 
-def test_compare_metrics_file(cwltool_setup):
+def test_compare_metrics_file():
     assert filecmp(
         "chr14_unfiltered_srt_abra_fm_alignment_metrics.txt",
         "test_bam_collapsing/test_output/chr14_unfiltered_srt_abra_fm_alignment_metrics.txt",
@@ -37,7 +88,6 @@ def test_compare_metrics_file(cwltool_setup):
         "chr14_unfiltered_srt_abra_fm-simplex_alignment_metrics.txt",
         "test_bam_collapsing/test_output/chr14_unfiltered_srt_abra_fm-simplex_alignment_metrics.txt",
     ), "File are not the same!"
-
 
 
 pytest.main()
