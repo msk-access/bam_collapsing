874f009740ad36e919542f22dfc460e75795c06f
Ronak Shah <rons.shah@gmail.com>
2019-09-06 16:00:05 -0400

    Removing Requirements from setup.py

--- setup.py
+++ setup.py
@@ -11,25 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    'pip==19.2.1'
-    'bumpversion==0.5.3'
-    'wheel==0.33.4'
-    'watchdog==0.9.0'
-    'flake8==3.7.8'
-    'tox==3.13.2'
-    'coverage==4.5.4'
-    'Sphinx==2.1.2'
-    'twine==1.13.0'
-    'pytest==5.0.1'
-    'pytest-runner==5.1'
-    'toil-ionox0[cwl]==0.0.7'
-    'pytz'
-    'typing==3.7.4'
-    'ruamel.yaml==0.15.77'
-    'travis-wait-improved==0.0.4'
-    'pytest-travis-fold==1.3.0'
-]
+requirements = []
 
 setup_requirements = [
     'pytest-runner', ]
